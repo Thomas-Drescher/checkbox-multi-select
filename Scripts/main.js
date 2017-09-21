8455ab7ea45b572f2130c7eb71208e6e036899e7
Tom Drescher <tmdrescher@yahoo.com>
2017-09-21 16:11:25 -0400

    moved CSS to stylesheets/styles.css

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,20 +5,21 @@ function handleCheck(e) {
   //Check if the shift key was down when clicked
   //AND check that the box was being checked, not unchecked
   let inBetween = false;
-  if(e.shiftKey && this.checked) {
+  if (e.shiftKey && this.checked) {
     //loop over every checkbox
     checkboxes.forEach(checkbox => {
       console.log(checkbox);
-      if(checkbox === this || checkbox === lastChecked){
+      if (checkbox === this || checkbox === lastChecked) {
         inBetween = !inBetween;
         console.log('starting to check the inBetween');
       }
-      if(inBetween) {checkbox.checked = true;
+      if (inBetween) {
+        checkbox.checked = true;
       }
     });
 
 
   }
-lastChecked = this;
+  lastChecked = this;
 }
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
